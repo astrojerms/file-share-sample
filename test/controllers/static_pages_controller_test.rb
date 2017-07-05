0475802c5cf6bbf43cda78ae1bd6473cce4bc3b0
jeremy <jeremy@host1.example.com>
2017-07-04 20:42:02 -0400

    add about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,22 +6,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "File Sharing"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title",  "#{@base_title}"
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
